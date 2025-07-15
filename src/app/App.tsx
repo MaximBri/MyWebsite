4f211589a66dfae0f4fdf11ee824c78c99cffd61
Maxim <MaximK38136@yandex.ru>
2025-07-15 17:49:48 +0400

    fix: name of import file

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ import { HashRouter } from 'react-router-dom'
 import { MainProvider } from './context/MainContext'
 import { AppRoutes } from './routes/AppRoutes'
 import './styles/Reset.css'
-import './styles/globals.scss'
+import './styles/Globals.scss'
 
 function App() {
   return (
