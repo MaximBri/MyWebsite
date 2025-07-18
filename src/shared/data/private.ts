38d717b5de45408bae515168f9d8596a0c6a9bed
Maxim <MaximK38136@yandex.ru>
2025-07-19 01:13:06 +0400

    feat: animation in header

--- src/shared/data/private.ts
+++ src/shared/data/private.ts
@@ -1,4 +1,4 @@
 export const KworkLink: string = 'https://kwork.ru/user/maxim_bars'
 export const TgLink: string = 'https://t.me/Maxim_Bars'
 export const GithubLink: string = 'https://github.com/MaximBri'
-export const TgGroupLink: string = 'https://t.me/+B_ZUosZK2zg1YjUy'
+export const TgGroupLink: string = 'https://t.me/FrontCraftGroup'
