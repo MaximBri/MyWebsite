9f4d647b4acfda5d4324af3a7b1d05593c1db383
Maxim <MaximK38136@yandex.ru>
2025-08-09 17:26:04 +0400

    fix: next.config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ import path from 'path'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: 'react',
   reactStrictMode: true,
   webpack: (config) => {
     config.resolve.alias = {
