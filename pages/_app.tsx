b232f6fdfd8e293b6d4a976a3d62e0d77c9b5ddf
Maxim <MaximK38136@yandex.ru>
2025-08-18 15:18:06 +0400

    chore: add meta info

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,8 +11,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta name='author' content='Maxim Bryzgalov' />
-        <meta http-equiv='Content-Language' content='ru' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta httpEquiv='Content-Language' content='ru' />
         <meta name='theme-color' content='#000000' />
         <meta
           name='description'
@@ -20,13 +19,39 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         />
         <meta
           name='keywords'
-          content='React, TypeScript, Redux, Redux Toolkit, MobX, Git, разработка, разработчик, сайт, Frontend, фронтенд, лендинг, вёртска'
+          content='React, TypeScript, Redux, Redux Toolkit, MobX, Git, разработка, разработчик, сайт, Frontend, фронтенд, лендинг, вёрстка'
         />
-        <title>FrontCraft</title>
+
+        <meta
+          property='og:title'
+          content='FrontCraft – Портфолио Frontend-разработчика'
+        />
+        <meta
+          property='og:description'
+          content='Искусство Frontend-разработки в каждом пикселе. Портфолио проектов на React/TypeScript с упором на оптимизацию, чистую архитектуру и UX.'
+        />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://front-craft.ru/' />
+        <meta property='og:image' content='/logo.png' />
+
+        <meta
+          name='twitter:title'
+          content='FrontCraft – Портфолио Frontend-разработчика'
+        />
+        <meta
+          name='twitter:description'
+          content='Искусство Frontend-разработки в каждом пикселе. Портфолио проектов на React/TypeScript с упором на оптимизацию, чистую архитектуру и UX.'
+        />
+        <meta name='twitter:image' content='/logo.png' />
+
         <link rel='icon' href='/favicon.ico' />
+
         <link rel='canonical' href='https://front-craft.ru/' />
+
         <link rel='stylesheet' href='/fonts/fonts.css' />
         <link rel='manifest' href='/manifest.json' />
+
+        <title>FrontCraft</title>
       </Head>
       <MainProvider>
         <MainLayout>
